262e040104f08f82208d1665c0a22201bcdeeae9
Brandon G.M <brandongmerritt@gmail.com>
2022-10-16 12:56:08 -0500

    folder figured

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          A quick clonable repo for your next React App. 
+          <br></br>
+          Please refer to the documentation below for offical documentation.
+          <br></br>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
